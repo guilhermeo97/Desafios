24d726c433af35d06560cf880859f096dd51b300
guilhermeo97 <55219644+guilhermeo97@users.noreply.github.com>
2024-01-12 11:57:22 -0300

    v2 - funções criadas

--- sistema_bancario/main.py
+++ sistema_bancario/main.py
@@ -21,50 +21,47 @@ def depositar():
     valor_extrato = str("Depsósito " + "R$ {:.2f}".format(valor_deposito))
     extrato.append(valor_extrato)
 
+def sacar():
+    global saldo
+    global num_saques
+    valor_saque = float(input("Digite o valor que você quer sacar: "))    
+    if valor_saque <= 0 or valor_saque > LIMITE_SAQUES_VALOR:
+        print("Valor inválido, digite um valor maior que R$ 0.00 e menor ou igual a R$ 500.00")
+        valor_saque = float(input("Digite o valor que você quer sacar: "))
+    elif valor_saque > saldo:
+        print("Valor de saque maior do que o saldo disponível em conta")
+        valor_saque = float(input("Digite o valor que você quer sacar: "))
+    elif num_saques > LIMITE_SAQUES_QTD:
+        print("Número de saques diários excedido!")
+    else:
+        saldo -= valor_saque
+        valor_extrato = str("Saque " + "R$ {:.2f}".format(valor_saque))
+        extrato.append(valor_extrato)
+        num_saques += 1
+
+def imprimir_extrato():
+    print()
+    if not extrato:
+        print("Não foram realizadas movimentações.")
+        print()
+        print(f"Seu saldo atual é de R$ {saldo:.2f}")
+    else:    
+        for i in extrato:
+            print(i)
+            print()
+            print(f"Seu saldo atual é de R$ {saldo:.2f}")
+
 while True:
     opcao = input("Digite a opção desejada: ")
     
     if opcao == "d":
-        depositar()
-        # valor_deposito = float(input("Digite o valor que você quer depositar: "))
-        # while valor_deposito <= 0:
-        #     print("Valor inválido, digite um valor maior que R$ 0,00")
-        #     valor_deposito = float(input("Digite o valor que você quer depositar: "))
-        # saldo += valor_deposito
-        # valor_extrato = str("Depsósito " + "R$ {:.2f}".format(valor_deposito))
-        # extrato.append(valor_extrato)
-        
-        
+        depositar()     
         
     elif opcao == "s":
-        
-        valor_saque = float(input("Digite o valor que você quer sacar: "))
-        
-        if valor_saque <= 0 or valor_saque > LIMITE_SAQUES_VALOR:
-            print("Valor inválido, digite um valor maior que R$ 0.00 e menor ou igual a R$ 500.00")
-            valor_saque = float(input("Digite o valor que você quer sacar: "))
-        elif valor_saque > saldo:
-            print("Valor de saque maior do que o saldo disponível em conta")
-            valor_saque = float(input("Digite o valor que você quer sacar: "))
-        elif num_saques > LIMITE_SAQUES_QTD:
-            print("Número de saques diários excedido!")
-        else:
-            saldo -= valor_saque
-            valor_extrato = str("Saque " + "R$ {:.2f}".format(valor_saque))
-            extrato.append(valor_extrato)
-            num_saques += 1
-            
-            
+        sacar()
+                     
     elif opcao == "e":
-        print()
-        if not extrato:
-            print("Não foram realizadas movimentações.")
-            print()
-            print(f"Seu saldo atual é de R$ {saldo:.2f}")
-        else:    
-            for i in extrato:
-                print(i)
-            print()
-            print(f"Seu saldo atual é de R$ {saldo:.2f}")
+        imprimir_extrato()
+
     else:
         break      
