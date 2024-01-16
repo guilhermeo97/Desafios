ac806dc93330d9d92ee2157bc07d859075b5b39e
guilhermeo97 <55219644+guilhermeo97@users.noreply.github.com>
2024-01-16 11:53:01 -0300

    v2 - cadastro de cliente

--- sistema_bancario/main.py
+++ sistema_bancario/main.py
@@ -1,15 +1,100 @@
 menu = print("""
         Bem vindo ao Sistema Bancário Banrisul
+        [c] cadastrar novo cliente
         [d] depósito
         [s] saque
         [e] extrato
         [q] sair""")
 
+
 extrato = []
+clientes = []
+cadastros_clientes = {}
 saldo = 0
 LIMITE_SAQUES_VALOR = 500
 LIMITE_SAQUES_QTD = 3
 num_saques = 1
+
+def cadastro_cliente():
+    global cadastros_clientes
+    if cadastros_clientes is None:
+        cpf_cliente = input("Digite o CPF do cliente: ")
+        while (len(cpf_cliente) < 11 and len(cpf_cliente) > 14):
+            print("O cpf digitado é inválido")
+            cpf_cliente = input("Digite o CPF do cliente: ")
+    else:
+        cpf_cliente = input("Digite o CPF do cliente: ")
+        while (len(cpf_cliente) < 11 and len(cpf_cliente) > 14) or cpf_cliente in cadastros_clientes:
+            print("O cpf digitado é inválido")
+            cpf_cliente = input("Digite o CPF do cliente: ")
+    
+    nome_cliente = input("Digite o nome completo do cliente: ")
+    while not nome_cliente or len(nome_cliente) <= 3:
+        print("O nome é obrigatório")
+        nome_cliente = input("Digite o nome completo do cliente: ")
+    
+    dt_nascimento_cliente = input("Digite a data de nascimento no formato 'dd/mm/aaaa': ")
+    while len(dt_nascimento_cliente) != 10:
+        print("Data de nascimento incorreta!")
+        nome_cliente = input("Digite a data de nascimento no formato 'dd/mm/aaaa': ")
+
+    endereco_logradouro_cliente = input("Digite o logradouro do cliente: ")
+    while not endereco_logradouro_cliente or len(endereco_logradouro_cliente) < 5:
+        print("O logradouro é obrigatório")
+        endereco_logradouro_cliente = input("Digite o logradouro do cliente: ")
+
+    endereco_numero_cliente = input("Digite o número do cliente: ")
+    while not endereco_numero_cliente:
+        print("O número é obrigatório")
+        endereco_logradouro_cliente = input("Digite o número do cliente: ")
+    
+    endereco_bairro_cliente = input("Digite o bairro do cliente: ")
+    while not endereco_bairro_cliente or len(endereco_bairro_cliente) <= 3:
+        print("O bairro é obrigatório")
+        endereco_bairro_cliente = input("Digite o bairro do cliente: ")
+
+    endereco_cidade_cliente = input("Digite a cidade do cliente: ")
+    while not endereco_cidade_cliente or len(endereco_cidade_cliente) <= 3:
+        print("A cidade é obrigatória")
+        endereco_cidade_cliente = input("Digite a cidade do cliente: ")
+    
+    endereco_uf_cliente = input("Digite a sigla do estado do cliente: ")
+    while len(endereco_uf_cliente) != 2:
+        print("A sigla do estado é obrigatória")
+        endereco_uf_cliente = input("Digite a sigla do estado do cliente: ")
+
+    endereco_unificado_cliente = f'{endereco_logradouro_cliente}, {endereco_numero_cliente} - {endereco_bairro_cliente} - {endereco_cidade_cliente}/{endereco_uf_cliente.upper()}'
+    
+    cadastros_clientes = {
+        cpf_cliente: {
+        'nome': nome_cliente,
+        'data_nascimento': dt_nascimento_cliente,
+        'endereco': endereco_unificado_cliente
+        }
+    }
+
+    clientes.append(cadastros_clientes)
+
+    print(clientes)
+
+def cadastrar_conta():
+    global cadastros_clientes
+    codigo_conta = str(len(cadastros_clientes) + 1)
+    if cadastros_clientes is None:
+        print("Não há clientes cadastrdos!")
+    else:
+        cpf_cliente = input("Digite o CPF do cliente: ")
+        while cpf_cliente not in cadastros_clientes:
+            print("O CPF digitado não foi encontrado")
+            cpf_cliente = input("Digite o CPF do cliente: ")
+        
+        usuario_cliente = input("Digite o nome de usuário do cliente: ")
+        while not usuario_cliente or len(usuario_cliente) <= 3:
+            print("O nome de usuário é obrigatório")
+            usuario_cliente = input("Digite o nome de usuário do cliente: ")
+        cadastros_clientes[cpf_cliente]   
+
+    
 
 def depositar():
     global saldo
@@ -50,7 +135,6 @@ def imprimir_extrato():
             print(i)
             print()
             print(f"Seu saldo atual é de R$ {saldo:.2f}")
-
 while True:
     opcao = input("Digite a opção desejada: ")
     
@@ -59,7 +143,9 @@ while True:
         
     elif opcao == "s":
         sacar()
-                     
+    elif opcao == "c":
+        cadastro_cliente()                 
+        cadastrar_conta()
     elif opcao == "e":
         imprimir_extrato()
 
